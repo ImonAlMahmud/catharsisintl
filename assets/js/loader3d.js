94f7fe2965922db1610228a7fcf491f30d5e92eb
Imon Mahmud <imon.mahmud4@gmail.com>
2026-08-10 15:04:18 +0600

    Remove temporary ring torus placeholder and render original 3D logo directly in loader

--- assets/js/loader3d.js
+++ assets/js/loader3d.js
@@ -245,13 +245,13 @@
   };
 
   var initialInterval = setInterval(function() {
-    if (!isDone && percent < 70) {
-      percent += 6;
-      if (percent > 70) percent = 70;
+    if (!isDone && percent < 88) {
+      percent += 4;
+      if (percent > 88) percent = 88;
       if (barFill) barFill.style.width = percent + '%';
       if (percentEl) percentEl.textContent = percent + '%';
     }
-  }, 16);
+  }, 20);
 
   function onLoaderModelReady() {
     loader3dLoaded = true;
@@ -337,15 +337,6 @@
     var mainGroup = new THREE.Group(); scene.add(mainGroup);
     var modelGroup = new THREE.Group(); mainGroup.add(modelGroup);
 
-    /* Instant 0ms Procedural 3D Catharsis Emblem Placeholder (Appears at 0% / Frame 1) */
-    var tempRingGeo = new THREE.TorusGeometry(1.25, 0.26, 16, 40);
-    var tempMatNavy = new THREE.MeshStandardMaterial({ color: 0x003087, roughness: 0.2, metalness: 0.8 });
-    var tempMesh = new THREE.Mesh(tempRingGeo, tempMatNavy);
-    modelGroup.add(tempMesh);
-
-    // Initial render call on Frame 1 (0ms) so 3D is immediately visible
-    renderer.render(scene, camera);
-
     if (typeof THREE.GLTFLoader !== 'undefined') {
       var loader = new THREE.GLTFLoader();
       loader.load(modelPath, function(gltf) {
@@ -357,11 +348,6 @@
 
         var c2 = box.getCenter(new THREE.Vector3());
         model.position.set(-c2.x * sz, -c2.y * sz, -c2.z * sz);
-
-        // Remove placeholder and insert loaded GLTF 3D model
-        modelGroup.remove(tempMesh);
-        if (tempRingGeo) tempRingGeo.dispose();
-        if (tempMatNavy) tempMatNavy.dispose();
 
         modelGroup.add(model);
 
